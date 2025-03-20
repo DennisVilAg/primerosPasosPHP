70413a6282dcece8cbe141f728aac29b05459410
dennis <dennisvilag@hotmail.com>
2025-03-20 18:02:39 +0100

    mejorando codigo

--- index.php
+++ index.php
@@ -1,66 +1,15 @@
 <?php
+require_once 'consts.php';
+require_once 'functions.php';
 
-const API_URL = "https://whenisthenextmcufilm.com/api";
-# Iniciailizar una nueva sesión de curl; ch = curl handle
-$ch = curl_init(API_URL);
-# Queremos recibir el resultado de la petición y no mostrarla en pantalla
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desactiva verificación SSL (solo para pruebas)
-# Ejecutamos la petición y guardamos el resultado
-$result = curl_exec($ch);
-$data = json_decode($result, true);
-
-curl_close($ch);
+$data = get_data(API_URL);
+$until_message = get_until_message($data["days_until"])
 
 ?>
 
-<head>
-    <meta charset="UTF-8"/>
-    <title>La próxima pelicula de Marvel</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
->
-</head>
-
-<main> 
-<h2 style="text-align: center;">Próxima pelicula de Marvel</h2>
-    <section>
-        
-        <img src="<?= $data["poster_url"]; ?>" width="300" alt="Poster de <?=$data["title"]; ?>"
-        style="border-radius: 16px;"/>
-    </section>
-
-    <hgroup>
-        <h3><?=$data["title"]; ?> se estrena en <?= $data["days_until"]; ?> días</h3>
-        <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
-        <p>La siguiente es: <?= $data["following_production"]["title"]; ?> </p>
-    </hgroup>
-    
-</main>
-
-<style>
-    :root{
-        color-scheme: light dark;
-    }
-
-    body{
-        display: grid;
-        place-content: center;
-    }
-
-    section{
-        display: flex;
-        justify-content: center;
-        text-align: center;
-    }
-
-    hgroup{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        text-align: center;
-    }
-
-</style>
+<?php render_template('head', $data); ?>
+<?php render_template('main', array_merge(
+    $data,
+    ["until_message" => $until_message]
+)) ?>
+<?php render_template('styles'); ?>
